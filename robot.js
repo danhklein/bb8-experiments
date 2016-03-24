b85ab627237fd7b51091f6b3f885697e0a3b86df
Daniel Klein <danhirschklein@gmail.com>
2016-03-23 20:35:23 -0600

    Added straight line/collision example

--- robot.js
+++ robot.js
@@ -1,9 +1,41 @@
-var Cylon = require('cylon');
+// var Cylon = require('cylon');
 
-Cylon.robot({
-  work: function() {
-    every((1).second(), function() {
-      console.log("Hello, human!");
-    });
-  }
-}).start();
+// Cylon.robot({
+//   work: function() {
+//     every((1).second(), function() {
+//       console.log("Hello, human!");
+//     });
+//   }
+// }).start();
+
+var sphero = require("sphero"),
+    bb8 = sphero("7c6dbda72e3c4096be4031ad9d63380c"); // change BLE address accordingly
+
+bb8.connect(function() {
+  // roll BB-8 in a random direction, changing direction every second
+  // setInterval(function() {
+  //   var direction = Math.floor(Math.random() * 360);
+  //   bb8.roll(150, direction);
+  // }, 1000);
+
+  bb8.roll(150, 0);
+
+   // turn Sphero green
+  bb8.color("green");
+
+  // have Sphero tell you when it detect collisions
+  bb8.detectCollisions();
+
+  // when Sphero detects a collision, turn red for a second, then back to green
+  bb8.on("collision", function(data) {
+    console.log("collision detected");
+    console.log("  data:", data);
+
+    bb8.color("red");
+
+    setTimeout(function() {
+      bb8.color("green");
+    }, 1000);
+  });
+});
+
