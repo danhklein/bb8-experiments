df49b21673aef2846051ab71082128693c7a4b99
Daniel Klein <danhirschklein@gmail.com>
2016-03-23 20:50:09 -0600

    Put collision detection within random movement function.

--- robot.js
+++ robot.js
@@ -13,15 +13,10 @@ var sphero = require("sphero"),
 
 bb8.connect(function() {
   // roll BB-8 in a random direction, changing direction every second
-  // setInterval(function() {
-  //   var direction = Math.floor(Math.random() * 360);
-  //   bb8.roll(150, direction);
-  // }, 1000);
-
-  bb8.roll(150, 0);
-
-   // turn Sphero green
-  bb8.color("green");
+  setInterval(function() {
+    var direction = Math.floor(Math.random() * 360);
+    bb8.roll(200, direction);
+    bb8.color("green");
 
   // have Sphero tell you when it detect collisions
   bb8.detectCollisions();
@@ -37,5 +32,26 @@ bb8.connect(function() {
       bb8.color("green");
     }, 1000);
   });
+  }, 1500);
+
+  // bb8.roll(150, 0);
+
+  //  // turn Sphero green
+  // bb8.color("green");
+
+  // // have Sphero tell you when it detect collisions
+  // bb8.detectCollisions();
+
+  // // when Sphero detects a collision, turn red for a second, then back to green
+  // bb8.on("collision", function(data) {
+  //   console.log("collision detected");
+  //   console.log("  data:", data);
+
+  //   bb8.color("red");
+
+  //   setTimeout(function() {
+  //     bb8.color("green");
+  //   }, 1000);
+  // });
 });
 
