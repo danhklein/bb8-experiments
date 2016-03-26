631f943ffc5d4b084361b75929f8b191bc7bdae9
Daniel Klein <danhirschklein@gmail.com>
2016-03-25 18:11:13 -0600

    Created color-robot.js

--- color-robot.js
+++ color-robot.js
@@ -6,17 +6,15 @@ var sphero = require("sphero"),
 bb8.connect(function() {
   // roll BB-8 in a random direction, changing direction every second
   setInterval(function() {
-    var direction = Math.floor(Math.random() * 360);
-    bb8.roll(250, direction);
+
+
     bb8.color("blue");
 
   // have Sphero tell you when it detect collisions
-  bb8.detectCollisions();
+
 
   // when Sphero detects a collision, turn red for a second, then back to green
-  bb8.on("collision", function(data) {
-    console.log("collision detected");
-    console.log("  data:", data);
+    bb8.color("yellow");
 
     bb8.color("red");
 
@@ -24,8 +22,7 @@ bb8.connect(function() {
       bb8.color("green");
     }, 5000);
   });
-  }, 1000);
+  }, 3000);
 
 
-});
 
